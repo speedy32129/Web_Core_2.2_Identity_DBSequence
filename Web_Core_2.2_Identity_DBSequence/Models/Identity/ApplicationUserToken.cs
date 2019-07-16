447e81ab669e87a66cb2b4c1c43b8a135ddffcc7
Jim McAleer <ironspeeder@hotmail.com>
2019-07-16 16:44:45 -0400

    Add project files.

---
+++ Web_Core_2.2_Identity_DBSequence/Models/Identity/ApplicationUserToken.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_Core_2._2_Identity_int.Models.Identity
+{
+    public class ApplicationUserToken : IdentityUserToken<int>
+    {
+    }
+}
